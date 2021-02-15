96f63b9184a5fa18ac56b43a81d6e61f239114f3
Shiritai <56602991+Shiritai@users.noreply.github.com>
2021-02-15 18:10:07 +0800

    rm *.exe
    remove executables

--- Stdlib_and_Preprocessing/tempCodeRunnerFile.c
+++
@@ -1,5 +0,0 @@
-p_ll_1 > tmp_ll_2))
-        return 1;
-    else
-       return -1;
-    return 0;
--- loop/tempCodeRunnerFile.c
+++
@@ -1,4 +0,0 @@
-<= sq_radius[0] \
-                    && (sq(x-circle[1][0]) + sq(y-circle[1][1])) <= sq_radius[1] \
-                    && (sq(x-circle[2][0]) + sq(y-circle[2][1])) <= sq_radius[2])
-                        counter[cases]++, printf("ALL\t(%lld, %lld)\n", x, y);
