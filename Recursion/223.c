f0dc428ecabd06793cb8679a01e1352724cc6ed5
Shiritai <56602991+Shiritai@users.noreply.github.com>
2021-02-16 04:31:34 +0800

    Update 223.c

--- Recursion/223.c
+++ Recursion/223.c
@@ -1,14 +1,14 @@
 /* 223. Tiles */ // edit version 裡 type 2, 4 寫反了... 哭阿
 # include <stdio.h>
 # define CENTER_TYPE 5
-
-typedef struct tile {
+/* a "strict" tile object */
+typedef struct tile { // "strict" tile object 的 「缺口大小」 等於 「長度的一半」，故僅需 len_ 表形狀
     int type_, len_, x_, y_;
 } tile;
-
-inline int Clk_trans(int a) {return (a == 1) ? 4 : a-1;}
-inline int ctClk_trans(int a) {return (a == 4) ? 1 : a+1;}
-
+/* rotate method */
+inline int Clk_trans(int a) {return (a == 1) ? 4 : a-1;} // rotate clockwise
+inline int ctClk_trans(int a) {return (a == 4) ? 1 : a+1;} // rotate counterclockwise
+/* generate component tiles */
 tile compo_gen(int type, tile mom){
     tile tmp;
     switch (type){
@@ -30,20 +30,20 @@ tile compo_gen(int type, tile mom){
     }
     return tmp;
 }
-
+/* main recur func */
 void print_tile(tile cur){
-    if (cur.len_ == 2){
+    if (cur.len_ == 2){ // a printable tile
         printf("%d %d %d\n", cur.type_, cur.x_, cur.y_);
         return;
     }
-    else {
+    else { // divide to smaller tiles
         print_tile(compo_gen(CENTER_TYPE, cur));
         print_tile(compo_gen(cur.type_, cur));
         print_tile(compo_gen(Clk_trans(cur.type_), cur));
         print_tile(compo_gen(ctClk_trans(cur.type_), cur));
     }
 }
-
+/* call tiles */
 void call_recur(int m, int center){
     while (center > 0){
         print_tile((tile) {.len_ = m * 2, .type_ = 1, .x_ = center, .y_ = center});
