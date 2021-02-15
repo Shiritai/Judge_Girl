8c00fa4ccf1de324a7f0cc32ca62fa8cb5457113
Shiritai <56602991+Shiritai@users.noreply.github.com>
2021-02-15 18:13:04 +0800

    remove files
    remove file irrelevant to Judge Girls

--- Free_test/tempCodeRunnerFile.c
+++
@@ -1,3 +0,0 @@
-
-    // printf("int pointer size: %d\n", sizeof(a_ptr));
-    // printf("float pointer size: %d\n", sizeof(f_ptr));
--- Free_test/test_.c
+++
@@ -1,9 +0,0 @@
-# include <stdio.h>
-# define Whole 8
-main(){
-    int i, j = 1;
-    for (i = 1; i <= Whole; i++)    j *= i;
-    printf("%d\n", j);
-    short short_test = 0x8000;
-    printf("%hd", short_test);
-}
