f77a66e4a7d6b997e96857008cc374e303121e60
Shiritai <56602991+Shiritai@users.noreply.github.com>
2021-04-15 15:26:49 +0800

    add Expression Tree

---
+++ Data_Structure/065_debug.c
@@ -0,0 +1,155 @@
+/* 65. Expression Tree */
+# include <stdio.h>
+# include <assert.h>
+# include <stdlib.h>
+# include <string.h>
+# include <ctype.h>
+
+typedef struct Node {
+    char dataOp;
+    int dataInt;
+    struct Node * left, * right;
+} Node;
+
+char * dataString;
+int readPos, readEnd;
+
+int readInt(const char * string){
+    int res;
+    sscanf(string, "%d", &res);
+    for (int i = 0; readPos != readEnd && string[i] != ' '; ++i, ++readPos);
+    return res;
+}
+
+void readBlankParenthesis(){
+    while (readPos != readEnd && (dataString[readPos] == ' ' || dataString[readPos] == '(' || dataString[readPos] == ')')){
+        ++readPos;
+    }
+    if (readPos == readEnd)
+        puts("Tis fuchong");
+}
+
+/* PreOrder */
+Node * build(Node * cur){
+    if (readPos != readEnd){
+        readBlankParenthesis();
+        /* Basis */
+        if (isdigit(dataString[readPos])){
+            if (cur == NULL){
+                cur = (Node *) malloc(sizeof(Node));
+            }
+            cur->dataInt = readInt(dataString + readPos);
+            printf("Read int : [%d]\n", cur->dataInt);
+            cur->left = cur->right = NULL;
+        }
+        /* Recursion */
+        else {
+            if (cur == NULL){
+                cur = (Node *) malloc(sizeof(Node));
+                cur->left = cur->right = NULL;
+            }
+            readBlankParenthesis();
+            cur->dataOp = dataString[readPos++];
+            printf("Read operator : [%c]\n", cur->dataOp);
+            readBlankParenthesis();
+            printf("Test char 1 : [%c]\n", dataString[readPos]);
+            cur->left = build(cur->left);
+            readBlankParenthesis();
+            printf("Test char 2 : [%c]\n", dataString[readPos]);
+            cur->right = build(cur->right);
+        }
+        return cur;
+    }
+    else {
+        puts("End of reading.");
+        return cur;
+    }
+    return NULL;
+}
+
+/* i.e. Compute the "cur" tree */
+int postOrderValue(Node * cur){
+    assert(cur != NULL);
+    if (cur->left == NULL && cur->right == NULL)
+        return cur->dataInt;
+    int data1 = postOrderValue(cur->left);
+    int data2 = postOrderValue(cur->right);
+    switch (cur->dataOp){
+        case '+':
+            return data1 + data2;
+        case '-':
+            return data1 - data2;
+        case '*':
+            return data1 * data2;
+        case '/':
+            return data1 / data2;
+        default:
+            puts("Error!");
+            return 0;
+    }
+}
+
+/* For debuging */
+void preOrder(Node * cur){
+    assert(cur != NULL);
+    if (cur->left == NULL && cur->right == NULL)
+        printf("%d\n", cur->dataInt);
+    else {
+        printf("%c\n", cur->dataOp);
+        preOrder(cur->left);
+        preOrder(cur->right);
+    }
+}
+
+char * str_certain_str(const char * string, const char * subStr){
+    char * tmpTarget = strstr(string, subStr);
+    while (tmpTarget != NULL && tmpTarget + strlen(subStr) != string){
+        if (tmpTarget[strlen(subStr)] == ' ' || tmpTarget[strlen(subStr)] == '\0'){
+            printf("Found : [%s]\n", tmpTarget);
+            return tmpTarget;
+        }
+        tmpTarget = strstr(tmpTarget + strlen(subStr), subStr);
+    }
+    return tmpTarget;
+}
+
+/* replace the repStr in source with repInt */
+void replace(char * source, char * repStr, int repInt, char * destination){
+    char * buf = (char *) malloc(sizeof(char) * strlen(destination));
+    char * target = str_certain_str(source, repStr);
+    assert(target != NULL);
+    int i = 0;
+    while (source + i != target)
+        buf[i] = source[i], i++;
+    buf[strlen(buf) - 1] = '\0'; // 記得下斷點，這樣 sprintf 才知道要讀到哪
+    while (source[i] != ' ')
+        ++i;
+    sprintf(buf, "%s %d%s", buf, repInt, source + i);
+    strcpy(destination, buf);
+}
+
+int main(void){
+    char str[1024], declaration[64], var[32];
+    int value;
+    fgets(str, 0x7fffffff, stdin);
+    str[strlen(str) - 1] = '\0';
+    while (fgets(declaration, 0x7fffffff, stdin) != NULL){
+        if (!strcmp(declaration, "-1\n")){
+            break;
+        }
+        sscanf(declaration, "%s = %d", var, &value);
+        while (str_certain_str(str, var) != NULL){
+            replace(str, var, value, str);
+            printf("Tmp res : [%s]\n", str);
+        }
+        // puts(dataString);
+    }
+    dataString = str, readPos = 0, readEnd = strlen(dataString); // initialize
+    puts(dataString);
+    Node * root = NULL;
+    root = build(root);
+    puts("Meow");
+    preOrder(root);
+    puts("Meow");
+    printf("%d\n", postOrderValue(root));
+}
--- Data_Structure/test.c
+++ Data_Structure/test.c
@@ -4,23 +4,38 @@
 # include <stdlib.h>
 # include <assert.h>
 
-/* replace the repStr in str with repInt */
-void replace(char * str, char * repStr, int repInt, char * destination){
-    memset(destination, 0, strlen(destination));
-    char * target = strstr(str, repStr);
+/* replace the repStr in source with repInt */
+void replace(char * source, char * repStr, int repInt, char * destination){
+    char * buf = (char *) malloc(sizeof(char) * strlen(destination));
+    char * target = strstr(source, repStr);
     assert(target != NULL);
     int i = 0;
-    while (str + i != target)
-        destination[i] = str[i], i++;
-    while (str[i] != ' ')
+    while (source + i != target)
+        buf[i] = source[i], i++;
+    buf[strlen(buf) - 1] = '\0'; // 記得下斷點，這樣 sprintf 才知道要讀到哪
+    while (source[i] != ' ')
         ++i;
-    sprintf(destination, "%s%d%s", destination, repInt, str + i);
+    sprintf(buf, "%s %d%s", buf, repInt, source + i);
+    strcpy(destination, buf);
 }
 
 int main(void){
     // test(2);
-    // char tmp[100] = "x_Meow21_456 = 110", * tmpPtr = tmp;
-    char tmp[1000] = "Meow?";
-    replace("( + 4 x_Meow )", "x_Meow", 100, tmp);
+    // char tmp[100] = "x_Meow2 = 110", * tmpPtr = tmp;
+    // char tmp[1000] = "Meow?";
+    char tmp[1000] = "( + _01234567890abcdefghijklmnopqrst ( + _01234567890abcdefghijklmnopqrst ( + _01234567890abcdefghijklmnopqrst ( + _01234567890abcdefghijklmnopqrst ( + _01234567890abcdefghijklmnopqrst ( + _01234567890abcdefghijklmnopqrst ( + _01234567890abcdefghijklmnopqrst ( + _01234567890abcdefghijklmnopqrst ( + _01234567890abcdefghijklmnopqrst ( + _01234567890abcdefghijklmnopqrst _01234567890abcdefghijklmnopqrst ) ) ) ) ) ) ) ) ) )";
+    int val;
+    char var[64];
+    sscanf("_01234567890abcdefghijklmnopqrst = 3\n", "%s = %d", var, &val);
+    puts(var);
+    while (strstr(tmp, var) != NULL)
+        replace(tmp, var, val, tmp);
+    // char tmp[1000] = "( + 4 x_Meow )";
+    // replace(tmp, "x_Meow", 100, tmp);
     puts(tmp);
+    // char tmp[100] = "123 eroiko, 124iwhrl8\n";
+    // int val = 0;
+
+    // sscanf(tmp, "%d", &val);
+    // printf("%d\n", val);
 }
